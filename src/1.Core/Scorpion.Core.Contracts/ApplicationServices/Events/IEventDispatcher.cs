2fca52f86ef37cb9f642a45bb062a40dca64f7bb
Mohammad Ramezani <34716164+mohrmz@users.noreply.github.com>
2022-07-09 19:56:35 +0430

    feet(contract) : Data in contracts

--- src/1.Core/Scorpion.Core.Contracts/ApplicationServices/Events/IEventDispatcher.cs
+++ src/1.Core/Scorpion.Core.Contracts/ApplicationServices/Events/IEventDispatcher.cs
@@ -1,6 +1,6 @@
 ﻿using Scorpion.Core.Domain.Events;
 
-namespace Scorpion.Core.ApplicationServices.Events;
+namespace Scorpion.Core.Contracts.ApplicationServices.Events;
 public interface IEventDispatcher
 {
     Task PublishDomainEventAsync<TDomainEvent>(TDomainEvent @event) where TDomainEvent : class, IDomainEvent;
