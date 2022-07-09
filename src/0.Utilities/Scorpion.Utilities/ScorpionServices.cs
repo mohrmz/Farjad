6af9392d18f52666546da938e8f4e8d539587c42
Mohammad Ramezani <34716164+mohrmz@users.noreply.github.com>
2022-07-09 15:23:42 +0430

    feet(Utilities) : Add Utilities Extensions

---
+++ src/0.Utilities/Scorpion.Utilities/ScorpionServices.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Zamin.Extensions.Caching.Abstractions;
+using Zamin.Extentions.ObjectMappers.Abstractions;
+using Zamin.Extentions.Serializers.Abstractions;
+using Zamin.Extentions.Translations.Abstractions;
+using Zamin.Extentions.UsersManagement.Abstractions;
+
+namespace Scorpion.Utilities
+{
+    public class ScorpionServices
+    {
+        public readonly ITranslator Translator;
+        public readonly ICacheAdapter CacheAdapter;
+        public readonly IMapperAdapter MapperFacade;
+        public readonly ILoggerFactory LoggerFactory;
+        public readonly IJsonSerializer Serializer;
+        public readonly IUserInfoService UserInfoService;
+
+        public ScorpionServices(ITranslator translator,
+                ILoggerFactory loggerFactory,
+                IJsonSerializer serializer,
+                IUserInfoService userInfoService,
+                ICacheAdapter cacheAdapter,
+                IMapperAdapter mapperFacade)
+        {
+            Translator = translator;
+            LoggerFactory = loggerFactory;
+            Serializer = serializer;
+            UserInfoService = userInfoService;
+            CacheAdapter = cacheAdapter;
+            MapperFacade = mapperFacade;
+        }
+    }
+}
---
+++ src/1.Core/Scoprion.Core.Domain.Toolkits/ValueObjects/NationalCode.cs
@@ -0,0 +1,52 @@
+﻿using Scorpion.Core.Domain.Exceptions;
+using Scorpion.Core.Domain.ValueObjects;
+using Scorpion.Utilities.Extensions;
+
+namespace Scoprion.Core.Domain.Toolkits.ValueObjects;
+
+public class NationalCode : BaseValueObject<NationalCode>
+{
+    #region Properties
+    public string Value { get; private set; }
+    #endregion
+
+    #region Constructors and Factories
+    public static NationalCode FromString(string value) => new(value);
+    public NationalCode(string value)
+    {
+        if (!value.IsNationalCode())
+        {
+            throw new InvalidValueObjectStateException("ValidationErrorStringFormat", nameof(NationalCode));
+        }
+
+        Value = value;
+    }
+    private NationalCode()
+    {
+
+    }
+    #endregion
+
+    #region Equality Check
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    #endregion
+
+    #region Operator Overloading
+
+    public static explicit operator string(NationalCode title) => title.Value;
+    public static implicit operator NationalCode(string value) => new(value);
+    #endregion
+
+    #region Methods
+    public override string ToString() => Value;
+
+    #endregion
+
+}
+
+
+
