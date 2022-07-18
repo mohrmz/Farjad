91fcd9e77f121b4cfba9e1f72af595a291387a52
Mohammad Ramezani <34716164+mohrmz@users.noreply.github.com>
2022-07-18 16:41:16 +0430

    style (BaseValueObject) : translate comments to english

--- src/1.Core/Scorpion.Core.Domain/ValueObjects/BaseValueObject.cs
+++ src/1.Core/Scorpion.Core.Domain/ValueObjects/BaseValueObject.cs
@@ -1,20 +1,35 @@
 ﻿namespace Scorpion.Core.Domain.ValueObjects
 {
     /// <summary>
-    /// کلاس پایه برای همه ValueObjectها
-    /// توضحیات کاملی در مورد دلایل وجود ValueObjectها را در لینک زیر مشاهده می‌کند
+    /// Base class for all Value Objects
+    /// A full explanation of the reasons for the existence of Value Objects can be found in the link below
     /// https://martinfowler.com/bliki/ValueObject.html
     /// </summary>
-    /// <typeparam name="TValueObject"></typeparam>
+    /// <typeparam name="TValueObject">Entity related base value object</typeparam>
+    
     public abstract class BaseValueObject<TValueObject> : IEquatable<TValueObject>
         where TValueObject : BaseValueObject<TValueObject>
     {
+        /// <summary>
+        /// Checks equality status
+        /// </summary>
+        /// <param name="other">Another value object for equlity check</param>
+        /// <returns>Equality status</returns>
         public bool Equals(TValueObject other) => this == other;
 
+        /// <summary>
+        /// Checks equality status
+        /// </summary>
+        /// <param name="obj">Another value object for equlity check</param>
+        /// <returns>Equality status</returns>
         public override bool Equals(object obj)
         {
             if (obj is TValueObject otherObject)
             {
+                if (this is null && otherObject is null)
+                    return true;
+                if (this is null || otherObject is null)
+                    return false;
                 return GetEqualityComponents().SequenceEqual(otherObject.GetEqualityComponents());
             }
             return false;
@@ -27,14 +42,14 @@
                 .Aggregate((x, y) => x ^ y);
         }
 
+        /// <summary>
+        /// Gets value object components.
+        /// </summary>
+        /// <returns>IEnumerable<object> Components</returns>
         protected abstract IEnumerable<object> GetEqualityComponents();
 
         public static bool operator ==(BaseValueObject<TValueObject> right, BaseValueObject<TValueObject> left)
         {
-            if (right is null && left is null)
-                return true;
-            if (right is null || left is null)
-                return false;
             return right.Equals(left);
         }
 
